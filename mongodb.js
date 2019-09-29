e3f4ddd0534e141fd2a2f5becf787b37003aba21
sadiq <sadiqmohammed234@gmail.com>
2019-09-29 13:21:13 +0530

    minor changes

--- mongodb.js
+++ mongodb.js
@@ -2,10 +2,13 @@
 
 const { MongoClient, ObjectID} = require('mongodb')
 
-
+const connectionURL='mongodb://127.0.0.1:27017'
+const databaseName='task-manager'
 const id = new ObjectID()
+
 console.log(id)
 console.log(id.getTimestamp())
+
 MongoClient.connect(connectionURL, {useNewUrlParser : true}, (error, client) => { 
     if(error) {
         return console.log('Unable to connect to database')
@@ -13,6 +16,7 @@ MongoClient.connect(connectionURL, {useNewUrlParser : true}, (error, client) => 
 
     const db = client.db(databaseName)
 
+    db.collection('users')
     
 })
 
